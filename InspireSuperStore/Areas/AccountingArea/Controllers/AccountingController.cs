4cd3027f0479907e6205ad37be9d1d9d5c00650f
Hammad <ceo@inspirealert.com>
2025-10-07 00:03:35 +0500

    Text of Text

---
+++ InspireSuperStore/Areas/AccountingArea/Controllers/AccountingController.cs
@@ -0,0 +1,94 @@
+﻿using MainModels.Models;
+using MainModels.Util;
+using MarketBal.Repository.AccountingRP;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InspireSuperStore.Areas.AccountingArea.Controllers
+{
+    [Authorize(Roles = UserRolesConstants.Accounts + "," + UserRolesConstants.Admin)]
+    [Area("AccountingArea")]
+    [Route("[controller]/[action]")]
+    public class AccountingController : Controller
+    {
+        private readonly ILogger<AccountingController> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly JournalsRepository _journalRepo;
+        private readonly ChartOfAccountRepo _chartOfAccountRepo;
+        private readonly OneDb _onedb;
+        public AccountingController(ILogger<AccountingController> logger, IConfiguration configuration,OneDb onedb)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _onedb = onedb;
+
+            _chartOfAccountRepo =new ChartOfAccountRepo(_configuration, _onedb);
+        }
+        public async Task<IActionResult> ChartOfAccounts()
+        {
+            var chartOfAccounts = await _chartOfAccountRepo.GetAllChartOfAccounts();
+            return View(chartOfAccounts);
+        }
+
+
+        public IActionResult JournalEntries()
+        {
+            return View();
+        }
+
+  
+        public IActionResult Receivables_Customers()
+        {
+            return View();
+        }
+
+        public IActionResult Receivables_Invoices()
+        {
+            return View();
+        }
+
+        public IActionResult Receivables_Payments()
+        {
+            return View();
+        }
+
+        public IActionResult Receivables_Statements()
+        {
+            return View();
+        }
+
+
+        public IActionResult Payables_Suppliers()
+        {
+            return View();
+        }
+
+        public IActionResult Payables_Bills()
+        {
+            return View();
+        }
+
+        public IActionResult Payables_Payments()
+        {
+            return View();
+        }
+
+        public IActionResult Payables_Statements()
+        {
+            return View();
+        }
+
+
+        public IActionResult Taxation()
+        {
+            return View();
+        }
+
+
+        public IActionResult Reconciliation()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ MarketBal/Repository/AccountingRP/ChartOfAccountRepo.cs
@@ -0,0 +1,36 @@
+﻿using MainModels;
+using MainModels.DTOModels;
+using MainModels.Models;
+
+namespace MarketBal.Repository.AccountingRP
+{
+    public class ChartOfAccountRepo
+    {
+        private readonly IConfiguration _config;
+        private readonly DBManager _db;
+        private readonly ApiMethods _api;
+        private readonly OneDb _onedb;
+        private readonly DapperContext _dap;
+        public ChartOfAccountRepo(IConfiguration config, OneDb oneDb)
+        {
+            _config = config;
+            _db = new DBManager(_config);
+            _api = new ApiMethods();
+            _onedb = oneDb;
+            _dap = new DapperContext(_config);
+        }
+        public async Task<List<ChartOfAccountVM>> GetAllChartOfAccounts()
+        {
+            try
+            {
+                var query = "SELECT * FROM ChartOfAccounts WHERE IsActive = 1 ORDER BY AccountNumber";
+                var result = await _db.GetDataListWithQueryAndParam<ChartOfAccountVM>(query);
+                return result.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error fetching chart of accounts.", ex);
+            }
+        }
+    }
+}
